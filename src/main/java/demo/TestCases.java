726991c181b564d69cb0dea84db7844bad56cb30
Neeru Kapoor <467neeru@gmail.com>
2024-05-08 19:26:35 +0530

    Fist commit

--- src/main/java/demo/TestCases.java
+++ src/main/java/demo/TestCases.java
@@ -1,6 +1,15 @@
 package demo;
 
+import java.util.Date;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 
@@ -13,6 +22,84 @@ public class TestCases {
         WebDriverManager.chromedriver().timeout(30).setup();
         driver = new ChromeDriver();
         driver.manage().window().maximize();
+        PageFactory.initElements(driver, this);
+    }
+
+    @FindBy(xpath = "//*[@id=\"mG61Hd\"]/div[2]/div/div[2]/div[1]/div/div/div[2]/div/div[1]/div/div[1]/input")
+    WebElement nameElement;
+
+    @FindBy(xpath = "//div[@class='Pc9Gce Wic03c']/textarea[@jsname='YPqjbf']")
+    WebElement textAreaElement;
+
+    @FindBy(xpath = "//div[@class='SG0AAe']/div[1]//div[@class='vd3tt']")
+    WebElement radioButtonElement;
+
+    @FindBy(xpath = "//div[@aria-labelledby='i25']/div/label/div/div/div[2]")
+    WebElement javaCheckboxElement;
+
+    @FindBy(xpath="//div[@aria-labelledby='i25']/div[2]/label/div/div/div[2]")
+    WebElement seleniumCheckboxElement;
+
+    @FindBy(xpath = "//div[@aria-labelledby='i25']/div[4]/label/div/div/div[2]")
+    WebElement testNGChckboxElement;
+
+    @FindBy(xpath = "//span[text()='Choose']")
+    WebElement dropdownElement;
+
+    @FindBy(xpath = "//div[@class='OA0qNb ncFHed QXL7Te']/div[3]")
+    WebElement selectGenderElement;
+
+    @FindBy(xpath = "(//div[@class='Xb9hP']/input[@class='whsOnd zHQkBf'])[3]")
+    WebElement hourElement;
+
+    @FindBy(xpath = "(//div[@class='Xb9hP']/input[@class='whsOnd zHQkBf'])[4]")
+    WebElement secondElement;
+
+
+    public  void testCase01() throws InterruptedException{
+        System.out.println("Start Test case: testCase01");
+        driver.get("https://docs.google.com/forms/d/e/1FAIpQLSep9LTMntH5YqIXa5nkiPKSs283kdwitBBhXWyZdAS-e4CxBQ/viewform");
+        nameElement.sendKeys("Dheeraj Kapoor");
+        long epoch= System.currentTimeMillis()/1000;
+        //Thread.sleep(2000);
+        textAreaElement.sendKeys("I want to be the best QA Engineer! "+epoch);
+        //Thread.sleep(2000);
+        radioButtonElement.click();
+        //Thread.sleep(2000);
+        javaCheckboxElement.click();
+        seleniumCheckboxElement.click();
+        testNGChckboxElement.click();
+        //Thread.sleep(2000);
+        JavascriptExecutor js= (JavascriptExecutor)driver;
+        js.executeScript("arguments[0].scrollIntoView(true);", dropdownElement);
+        //Thread.sleep(2000);
+        dropdownElement.click();
+        Thread.sleep(2000);
+        selectGenderElement.click();
+        DateFormat dm= new SimpleDateFormat("HH");
+        Date d= new Date();
+        String time1= dm.format(d);
+        hourElement.sendKeys(time1);
+        
+        DateFormat dm1= new SimpleDateFormat("mm");
+        Date d1= new Date();
+        String time2= dm1.format(d1);
+        secondElement.sendKeys(time2);
+        //Thread.sleep(3000);
+        driver.navigate().to("https://www.amazon.in/");
+        Thread.sleep(3000);
+
+        driver.switchTo().alert().dismiss();
+        Thread.sleep(3000);
+
+        driver.findElement(By.xpath("//span[text()='Submit']")).click();
+
+
+        
+
+
+
+        System.out.println("end Test case: testCase01");
     }
 
     public void endTest()
@@ -21,13 +108,6 @@ public class TestCases {
         driver.close();
         driver.quit();
 
-    }
-
-    
-    public  void testCase01(){
-        System.out.println("Start Test case: testCase01");
-        driver.get("https://www.google.com");
-        System.out.println("end Test case: testCase02");
     }
 
 
